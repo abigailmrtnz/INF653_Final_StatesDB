94a3351819912a63a819d8619ae37dd3e87697a7
amabbymrtnz <amabigailmrtnz@gmail.com>
2023-05-03 21:00:25 -0700

    error handling

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-require('dotenv').config();
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {};
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('log', (msg) => logEvents(msg));
+
+myEmitter.emit('log', 'Log event emitted!');
