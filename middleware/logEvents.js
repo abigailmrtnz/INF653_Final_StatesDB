6dd2355e507c3a13b22b01b38e3c330cabe041e4
amabbymrtnz <amabigailmrtnz@gmail.com>
2023-05-04 21:29:14 -0700

    cleaning up

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -1,7 +1,6 @@
 const { format } = require('date-fns');
 const { v4: uuid } = require('uuid');
 
-//import common core modules
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
@@ -9,13 +8,14 @@ const path = require('path');
 const logEvents = async (message, logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-    console.log(logItem);
+
     try {
         if (!fs.existsSync(path.join(__dirname, '..', 'logs'))) {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
+
         await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
-    }catch (err) {
+    } catch (err) {
         console.log(err);
     }
 }
