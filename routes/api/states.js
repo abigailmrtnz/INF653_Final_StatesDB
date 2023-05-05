23a6034035fbbc7d3c20f6f0648636a7b135c69b
amabbymrtnz <amabigailmrtnz@gmail.com>
2023-05-05 16:25:19 -0600

    error fixing

--- routes/api/states.js
+++ routes/api/states.js
@@ -1,43 +1,14 @@
-const express = require('express');
-const router = express.Router();
-const statessController = require('../../controllers/statesController');
-
-router.route('/')
-    .get(statessController.getAllEmployees)
-    .post(statessController.createNewEmployee)
-    .put(statessController.updateEmployee)
-    .delete(statessController.deleteEmployee);
-
-router.route('/:id')
-    .get(employeesController.getEmployee);
-
-module.exports = router;
-
-
-//routes
-router.route('/')
-    .get(statesController.getAllStates)
-
-router.route('/:state')
-        .get(statesController.getState);
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+//schema for mongodb
+const StatesSchema = new Schema({
+     statecode: {
+        type: String, 
+        required: true,
+        unique: true
+    },
+    funfacts: [String] 
     
-router.route('/:state/funfact')
-        .get(statesController.getFunFact)
-        .post(statesController.createFunFact)
-        .patch(statesController.updateFunFact)
-        .delete(statesController.deleteFunFact); 
+});
 
-router.route('/:state/capital')
-        .get(statesController.getCapital);
-
-router.route('/:state/nickname')
-        .get(statesController.getNickname);
-
-router.route('/:state/population')
-        .get(statesController.getPopulation);
-
-router.route('/:state/admission')
-        .get(statesController.getAdmission);
-
-
-module.exports = router;
+module.exports = mongoose.model('State', StatesSchema);
--- server.js
+++ server.js
@@ -26,20 +26,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
 app.use('/states', require('./routes/api/states'));
 
-app.all('*', (req, res) => {
+app.get('*', (req, res) => {
     res.status(404);
     if (req.accepts('html')) {
         res.sendFile(path.join(__dirname, 'views', '404.html'));
     } else if (req.accepts('json')) {
         res.json({ "error": "404 Not Found" });
     } else {
-        res.type('txt').send("404 Not Found");
+        res.type('text').send("404 Not Found");
     }
 });
 
